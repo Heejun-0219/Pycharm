d693d2a5b25a06547ec678bfc8724cbe32cbf23d
Heejun-0219 <heejun0219@gmail.com>
2023-05-15 21:24:00 +0900

    21

--- test123/pragmatic/accountapp/views.py
+++ test123/pragmatic/accountapp/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
-
 
 # Create your views here.
 def hello_world(request):
@@ -11,16 +13,27 @@ def hello_world(request):
     # return HttpResponse('Hello World')
     # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
 
+    # POST | GET
     if request.method == "POST":
         tmp = request.POST.get('hello_world_input')
         world = HelloWorld()
         world.text = tmp
         world.save()
 
-        hello_world_objects_all = HelloWorld.objects.all()
+        hello_world_objects_all = HelloWorld.objects.all() # django ORM - 객체 (ORM이란?)
         # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_objects_all})
-        return HttpResponseRedirect(reverse('accoutapp:hello_world'))
+        return HttpResponseRedirect(reverse('accoutapp:hello_world')) # redirect get을 요청 // 다시 함수 호출
     else:
         hello_world_objects_all = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_objects_all})
 
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    # 계정 만드는 것에 성공했다면 어떤 주소로 다시 돌려줄 것인가 지정
+    # reverse | reverse_lazy 클래스 내에서는 reverse를 바로 사용할 수 없다
+    # reverse = 함수형 뷰
+    # reverse_lazy = 클래스형 뷰
+    success_url = reverse_lazy('account:hello_world')
+    template_name = 'accountapp/create.html'
