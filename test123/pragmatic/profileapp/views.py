3b8d0fe38532acc455d06a03e98f2252d5da5705
Heejun-0219 <heejun0219@gmail.com>
2023-05-23 21:38:46 +0900

    articleapp

--- test123/pragmatic/profileapp/views.py
+++ test123/pragmatic/profileapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorator import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
-
 
 # Create your views here.
 
@@ -12,7 +13,8 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'targer_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # detail에서 pk를 확인하기 때문에 다음 명령어가 바로 실행되지 않는다.
+    # success_url = reverse_lazy('accountapp:detail')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -21,3 +23,18 @@ class ProfileCreateView(CreateView):
         tmp_profile.save()
 
         return super().form_valid(form)
+
+    def get_success_url(self):
+      return reverse('accountapp:detail', kwargs={'pk' : self.object.user.pk})
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'targer_profile'
+    form_class = ProfileCreationForm
+    # success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+      return reverse('accountapp:detail', kwargs={'pk' : self.object.user.pk})
